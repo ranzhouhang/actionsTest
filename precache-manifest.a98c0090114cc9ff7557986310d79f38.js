0f15b0329b31ee4b867ad0dd7210a5935b601694
ranzhouhang <81380@163.com>
2020-10-22 06:09:58 +0000

    Deploying to gh-pages from master 160a2d2e5d7a9cc36e8a6dd4394a5ccec585a933

---
+++ precache-manifest.a98c0090114cc9ff7557986310d79f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ad50728739d3cab1c1a5c2a27f0f50",
+    "url": "/actionsTest/index.html"
+  },
+  {
+    "revision": "2604f506c1b95507dbf7",
+    "url": "/actionsTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c493f7c1dc35ea0b1a94",
+    "url": "/actionsTest/static/js/2.bf5d9b3f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/actionsTest/static/js/2.bf5d9b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2604f506c1b95507dbf7",
+    "url": "/actionsTest/static/js/main.29ff3911.chunk.js"
+  },
+  {
+    "revision": "89d701a76d2497fd6428",
+    "url": "/actionsTest/static/js/runtime-main.fc52632c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/actionsTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
